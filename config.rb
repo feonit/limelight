20c774ecbaf6b3cdfa7157f9e588154aedde6493
Feonit <feonitu@yandex.ru>
2016-09-07 18:17:05 +0300

    update

--- config.rb
+++ config.rb
@@ -1,3 +1,22 @@
-css_dir = './src/style'
-sass_dir = './src/style'
-scss_dir = './src/style'
+environment = 'development'
+
+project_path = './'
+
+# public
+css_path = './build/stylesheets'
+generated_images_path = './build/images'
+
+# they not copy to build dir
+http_fonts_path = './../../src/style/fonts'
+
+http_images_path = './../../src/images'
+http_generated_images_path = './../images'
+
+sass_path = './src/style/scss'
+images_path = './src/images'
+fonts_path = './src/style/fonts'
+
+sourcemap = true
+disable_warnings = false
+
+output_style = 'expanded'
